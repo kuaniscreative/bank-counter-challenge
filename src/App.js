f21f35275e92436eec116fae787711947e40308c
kuaniscreative <benben19911020@gmail.com>
2022-05-17 17:11:21 +0800

    feat(app) apply bank-board and complete layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
+import { Bank } from "./core/bank/bank";
+import { useBehaviorSubjectValue } from "./hooks/useBehaviorSubjectValue";
+import BankBoard from "./core/bank/components/BankBoard/BankBoard";
+import Button from "./ui/Button/Button";
+import 'modern-normalize';
+import './global.scss';
+import classes from './app.module.scss';
+
+const bank = new Bank({
+  counterCount: 4
+});
+
 function App() {
+
+  const taskQueue = useBehaviorSubjectValue(bank.taskQueue$);
+  const nextTask = useBehaviorSubjectValue(bank.nextTask$);
+
   return (
-    <div className="App">
-      bank challenge
+    <div className={classes.root}>
+      <div className={classes.body}>
+        <h1 className={classes.title}>
+          Bank Counter
+        </h1>
+        <BankBoard counters={bank.counters}/>
+      </div>
+      <footer className={classes.footer}>
+        <div>
+          {`Waiting: ${taskQueue.length}`}
+        </div>
+        <Button 
+          onClick={() => {
+            bank.createNewTask();
+          }}
+        >
+          {`NEXT: ${nextTask}`}
+        </Button>
+      </footer>
     </div>
   );
 }
